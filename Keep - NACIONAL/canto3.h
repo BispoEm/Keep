4d7ca4bdb9ee0a8d61a5c05884f76488d807e839
Duarda29 <113449466+Duarda29@users.noreply.github.com>
2022-09-23 15:35:24 -0400

    add Maria  - 23.09.22
    Alterei a velocidade dos comandos porque estava indo muito rápido, assim, mudei os valores de tempo dos comandos. E dei também uma continuidade, finalizando a a primeira limpeza do canto3, para partir para  a segunda. Mas após a finalização do canto em si, ajustarei qualquer errinho.

--- Keep - NACIONAL/canto3.h
+++ Keep - NACIONAL/canto3.h
@@ -4,12 +4,12 @@ void canto3()
 
 
 	//dar uma re
-	setMultipleMotors(-50, motorA, motorB);
+	setMultipleMotors(-40, motorA, motorB);
 	wait(3500, milliseconds);
 	stopAllMotors();
 
 	//ir para frente
-	setMultipleMotors(50, motorA, motorB);
+	setMultipleMotors(40, motorA, motorB);
 	wait(300, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
@@ -20,106 +20,119 @@ void canto3()
 	wait(100, milliseconds);
 
 	//virar um tiquinho para esquerda
-	setMotor(motorA, -40);
-	setMotor(motorB, 50);
+	setMotor(motorA, -30);
+	setMotor(motorB, 40);
 	wait(400, milliseconds);
 	stopAllMotors();
 
 	//ir para frente
-	setMultipleMotors(50, motorA, motorB);
+	setMultipleMotors(40, motorA, motorB);
 	wait(850, milliseconds);
 	stopAllMotors();
 
 
 	//virar um tiquinho para direita
-	setMotor(motorB, -45);
-	setMotor(motorA, 50);
-	wait(150, milliseconds);
+	setMotor(motorB, -35);
+	setMotor(motorA, 40);
+	wait(200, milliseconds);
 	stopAllMotors();
 
 
 	//ir para frente
-	setMultipleMotors(50, motorA, motorB);
-	wait(210, milliseconds);
-	stopAllMotors();
-
-	//ir meio torto para esquerda enquanto vai para frente
-	setMotor(motorA, 45);
-	setMotor(motorB, 50);
-	wait(2500, milliseconds);
-	stopAllMotors();
-
-	//virar um tiquinho para direita
-	setMotor(motorB, -45);
-	setMotor(motorA, 50);
+	setMultipleMotors(40, motorA, motorB);
 	wait(400, milliseconds);
 	stopAllMotors();
 
-
 	//ir meio torto para esquerda enquanto vai para frente
-	setMotor(motorA, 45);
-	setMotor(motorB, 50);
-	wait(1100, milliseconds);
+	setMotor(motorA, 35);
+	setMotor(motorB, 40);
+	wait(2500, milliseconds);
 	stopAllMotors();
 
 
 	//virar um tiquinho para direita
-	setMotor(motorB, -45);
-	setMotor(motorA, 50);
-	wait(250, milliseconds);
+	setMotor(motorB, -35);
+	setMotor(motorA, 40);
+	wait(450, milliseconds);
 	stopAllMotors();
+
+
+	//ir meio torto para esquerda enquanto vai para frente
+	setMotor(motorA, 35);
+	setMotor(motorB, 40);
+	wait(1900, milliseconds);
+	stopAllMotors();
+
+
+	//virar um tiquinho para direita
+	setMotor(motorB, -35);
+	setMotor(motorA, 40);
+	wait(300, milliseconds);
+	stopAllMotors();
+	wait(200, milliseconds);
 
 
 
 	//ir meio torto para esquerda enquanto vai para frente
-	setMotor(motorA, 45);
-	setMotor(motorB, 50);
-	wait(3500, milliseconds);
+	setMotor(motorA, 35);
+	setMotor(motorB, 40);
+	wait(4600, milliseconds);
 	stopAllMotors();
 
 
 	//dar uma re
-	setMultipleMotors(-50, motorA, motorB);
+	setMultipleMotors(-40, motorA, motorB);
 	wait(450, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
 
 
 	//ir para frente
-	setMultipleMotors(50, motorA, motorB);
-	wait(550, milliseconds);
+	setMultipleMotors(40, motorA, motorB);
+	wait(800, milliseconds);
 	stopAllMotors();
 
 
 	//dar uma r?
-	setMultipleMotors(-50, motorA, motorB);
+	setMultipleMotors(-40, motorA, motorB);
 	wait(1900, milliseconds);
 	stopAllMotors();
 
 	//virar um tiquinho para direita
-	setMotor(motorB, -35);
-	setMotor(motorA, 50);
+	setMotor(motorB, -30);
+	setMotor(motorA, 40);
 	wait(350, milliseconds);
 	stopAllMotors();
 	wait(200, milliseconds);
 
 
 	//dar uma r?
-	setMultipleMotors(-50, motorA, motorB);
+	setMultipleMotors(-40, motorA, motorB);
 	wait(900, milliseconds);
 	stopAllMotors();
 
-	//rÃÂÃÂ© com o motor b
-	setMotor(motorB, -50);
-	setMotor(motorA, -25);
-	wait(700, milliseconds);
+	//rÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚ÂƒÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚ÂƒÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â‚ÃƒÂƒÃ‚Â
+	setMotor(motorB, -40);
+	setMotor(motorA, -20);
+	wait(800, milliseconds);
 	stopAllMotors();
+
+	//ir para frente
+	setMultipleMotors(40, motorA, motorB);
+	wait(400, milliseconds);
+	stopAllMotors();
+
+	//ir para tras
+	setMultipleMotors(-40, motorA, motorB);
+	wait(800, milliseconds);
+	stopAllMotors();
+
 
 	//levanta a garra
 	setMotor(motorC,60);
-	moveMotor(motorC,130,degrees,60);
+	moveMotor(motorC,140,degrees,60);
 	stopAllMotors();
-	wait(200,milliseconds);
+	wait(500,milliseconds);
 
 
 	//baixar garra
@@ -131,21 +144,22 @@ void canto3()
 	setMotor(motorC,60);
 	moveMotor(motorC,140,degrees,60);
 	stopAllMotors();
-	wait(200,milliseconds);
+	wait(400,milliseconds);
 
 
 	//////////////////////////
 
 	//ir para frente
-	setMultipleMotors(50, motorA, motorB);
+	setMultipleMotors(40, motorA, motorB);
 	wait(900, milliseconds);
 	stopAllMotors();
 
 	//virar um tiquinho para esquerda
-	setMotor(motorA, -40);
-	setMotor(motorB, 50);
-	wait(500, milliseconds);
+	setMotor(motorA, -35);
+	setMotor(motorB, 40);
+	wait(565, milliseconds);
 	stopAllMotors();
+
 
 	//baixar garra
 	baixarGarra();
@@ -153,32 +167,45 @@ void canto3()
 	wait(100, milliseconds);
 
 	//ir para frente
-	setMultipleMotors(50, motorA, motorB);
-	wait(900, milliseconds);
+	setMultipleMotors(40, motorA, motorB);
+	wait(1300, milliseconds);
 	stopAllMotors();
 
 	//ir para tras
-	setMultipleMotors(-50, motorA, motorB);
+	setMultipleMotors(-40, motorA, motorB);
+	wait(700, milliseconds);
+	stopAllMotors();
+	wait(300,milliseconds);
+
+		//levanta a garra
+	setMotor(motorC,60);
+	moveMotor(motorC,90,degrees,60);
+	stopAllMotors();
+	wait(200,milliseconds);
+
+
+	//virar um tiquinho para direita
+	setMotor(motorB, -30);
+	setMotor(motorA, 40);
 	wait(600, milliseconds);
 	stopAllMotors();
 
 
-	//virar um tiquinho para direita
-	setMotor(motorB, -40);
-	setMotor(motorA, 50);
-	wait(400, milliseconds);
+	//baixar garra
+	baixarGarra();
 	stopAllMotors();
+	wait(100, milliseconds);
 
 	//ir para frente torto para direita
-	setMotor(motorA, 50);
-	setMotor(motorB, 45);
-	wait(2800, milliseconds);
+	setMotor(motorA, 40);
+	setMotor(motorB, 35);
+	wait(2900, milliseconds);
 	stopAllMotors();
 
 
 	// ir para frente
-	setMotor(motorA, 40);
-	setMotor(motorB, 40);
+	setMotor(motorA, 30);
+	setMotor(motorB, 30);
 	wait(4000, milliseconds);
 	stopAllMotors();
 	wait(100,milliseconds);
@@ -188,48 +215,49 @@ void canto3()
 
 
 	//dar uma mini r?
-	setMultipleMotors(-50, motorA, motorB);
+	setMultipleMotors(-40, motorA, motorB);
 	wait(280, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
 
 
 	// ir para frente
-	setMotor(motorA, 45);
+	setMotor(motorA, 35);
 	setMotor(motorB, 25);
 	wait(900, milliseconds);
 	stopAllMotors();
 	wait(100,milliseconds);
 
 	//dar uma r?
-	setMultipleMotors(-50, motorA, motorB);
+	setMultipleMotors(-40, motorA, motorB);
 	wait(500, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
+
 	//levanta a garra
-	setMotor(motorC,60);
-	moveMotor(motorC,100,degrees,60);
+	setMotor(motorC,30);
+	moveMotor(motorC,110,degrees,30);
 	stopAllMotors();
 	wait(100,milliseconds);
 
 	//dar uma r?
-	setMultipleMotors(-50, motorA, motorB);
-	wait(1400, milliseconds);
+	setMultipleMotors(-40, motorA, motorB);
+	wait(1550, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
 
 	//////
 
 	//virar para direita
-	setMotor(motorA,50);
-	setMotor(motorB, -50);
+	setMotor(motorA,40);
+	setMotor(motorB, -40);
 	wait(680, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
 
 
 	//ir para frente
-	setMotor(motorA,50);
+	setMotor(motorA,40);
 	setMotor(motorB,35);
 	wait(500, milliseconds);
 	stopAllMotors();
@@ -237,20 +265,38 @@ void canto3()
 
 
 	//virar para direita
-	setMotor(motorA,50);
-	setMotor(motorB, -50);
+	setMotor(motorA,40);
+	setMotor(motorB, -40);
 	wait(600, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
 
 
-	//alinhar para tr�
-	setMultipleMotors(-50, motorA, motorB);
+	//alinhar para trÃƒ
+	setMultipleMotors(-40, motorA, motorB);
 	wait(2000, milliseconds);
 	stopAllMotors();
-	wait(1000000000000, milliseconds);
+	wait(100, milliseconds);
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////Se tiver obstaculo na frente, fazer o if para isso a partir dessa linha de partida do codigo/////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
+	displayCenteredBigTextLine(5,"dist3 %d",Dist_Frente());
+	wait(55, milliseconds);
+	if (Dist_Frente() >= 25 ){ //triangulo esta no canto 3
+	setLEDColor(ledOrangePulse);
+	playSound(soundBeepBeep);
+	canto3Obstaculo();
+	*/
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
 	//baixar garra
 	baixarGarra();
@@ -258,15 +304,15 @@ void canto3()
 	wait(100, milliseconds);
 
 	//ir  para frente
-	setMultipleMotors(50, motorA, motorB);
-	wait(3700, milliseconds);
+	setMultipleMotors(40, motorA, motorB);
+	wait(3900, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
 
 
 	//ir um pouco para tras
-	setMultipleMotors(-50, motorA, motorB);
-	wait(800, milliseconds);
+	setMultipleMotors(-40, motorA, motorB);
+	wait(900, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
 
@@ -277,43 +323,54 @@ void canto3()
 	wait(100,milliseconds);
 
 	//virar para esquerda
-	setMotor(motorB,50);
-	setMotor(motorA,-50);
-	wait(1300, milliseconds);
+	setMotor(motorB,40);
+	setMotor(motorA,-40);
+	wait(1200, milliseconds);
 	stopAllMotors();
 	wait(100, milliseconds);
 
 	//dar uma r?
-	setMultipleMotors(-50, motorA, motorB);
-	wait(2400, milliseconds);
+	setMultipleMotors(-40, motorA, motorB);
+	wait(3000, milliseconds);
+	stopAllMotors();
+	wait(10, milliseconds);
+
+	/////levanta a garra
+	setMotor(motorC,60);
+	moveMotor(motorC,120,degrees,60);
+	stopAllMotors();
+	wait(500,milliseconds);
+
+
+	//baixar garra
+	baixarGarra();
 	stopAllMotors();
 	wait(100, milliseconds);
 
 	//levanta a garra
-	setMotor(motorC,30);
-	moveMotor(motorC,80,degrees,30);
+	setMotor(motorC,60);
+	moveMotor(motorC,120,degrees,60);
 	stopAllMotors();
-	wait(1000,milliseconds);
+	wait(400,milliseconds);
 
 	//baixar garra
 	baixarGarra();
-
-	//levanta a garra
-	setMotor(motorC,40);
-	moveMotor(motorC,170,degrees,40);
 	stopAllMotors();
-	wait(2000,milliseconds);
-
-	//baixar garra
-	baixarGarra();
+	wait(100, milliseconds);
 
 	///////////////////////////////////segunda limpeza da pista///////////////////////////////////
 
 	//ir um pouco para frente
-	setMultipleMotors(50, motorA, motorB);
+	setMultipleMotors(40, motorA, motorB);
 	wait(600, milliseconds);
 	stopAllMotors();
-	wait(100, milliseconds);
+	wait(1000000000000000, milliseconds);
+
+
+	/*
+
+
+
 
 
 	//gira 45 graus
@@ -483,7 +540,6 @@ void canto3()
 	stopAllMotors();
 	wait(100, milliseconds);
 	*/
-
 
 
 
